4fedc573c4c5aa5277e64b2c0e4e1126538764e4
Dean Nguyen <d.v.q.nguyen@student.tudelft.nl>
2020-11-30 13:09:55 +0100

    Add the BikeRepair assignment

---
+++ src/Greedy/BikeRepair.java
@@ -0,0 +1,115 @@
+package Greedy;
+
+import java.io.InputStream;
+import java.util.*;
+
+public class BikeRepair {
+
+    /*
+Mike runs a bike repair service for students. Students can plan a time to bring in their bike on his website, where they
+also enter a description of what is broken. Mike is very good at his job and he can flawlessly predict how long each repair
+will cost. To cater the need of students to have a working bike at all time, he promises his customers to finish the repair
+as soon as possible after the bike is brought in.
+
+Now Mike wants to know how many of his employees to schedule for a day. He has the list of repairs for this day and he wants
+you to develop an algorithm to tell him how many people he needs.
+
+You get the list in the following format: On the first line is the amount of repairs.
+The next lines list the repairs with on each line the time the repair starts followed by the time the repair will take.
+     */
+    public String solve() {
+        // TODO
+        InputStream input = getClass().getResourceAsStream("bike.in");
+        Scanner sc = new Scanner(input);
+
+        int n = sc.nextInt();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+
+        Interval[] intervals = new Interval[n];
+
+        //Keep the labeling of the intervals and the end time
+        List<Integer> labelsAndEndTime = new ArrayList<>();
+        labelsAndEndTime.add(Integer.MIN_VALUE);
+
+        for(int i = 0; i < n; i++)
+        {
+            starts[i] = sc.nextInt();
+            ends[i] = starts[i] + sc.nextInt();
+            intervals[i] = new Interval(starts[i],ends[i]);
+        }
+
+        System.out.println("Old Intervals");
+        for(Interval interval: intervals)
+        {
+            System.out.println(interval.start + ":" + interval.end);
+        }
+
+        Arrays.sort(intervals);
+        //Arrays.sort(ends);
+        System.out.println();
+        System.out.println("Sorted Intervals");
+        for(Interval interval: intervals)
+        {
+            System.out.println(interval.start + ":" + interval.end);
+        }
+
+        for (Interval interval: intervals)
+        {
+            for(int i = 0; i < labelsAndEndTime.size(); i++)
+            {
+                if(interval.start >=  labelsAndEndTime.get(i))
+                {
+                    labelsAndEndTime.set(i,interval.end);
+                    interval.labeled = true;
+                    break;
+                }
+            }
+            if(!interval.labeled)
+            {
+                labelsAndEndTime.add(interval.end);
+            }
+        }
+
+
+        return String.valueOf(labelsAndEndTime.size());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BikeRepair().solve());
+    }
+}
+
+class Interval implements Comparable<Interval> {
+
+    public int start;
+
+    public int end;
+
+    public boolean labeled;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Interval)) return false;
+        Interval interval = (Interval) o;
+        return start == interval.start &&
+                end == interval.end;
+    }
+
+
+    @Override
+    public int compareTo(Interval o) {
+        int res = Integer.signum(this.start - o.start);
+        if (res == 0) {
+            return Integer.signum(this.end - o.end);
+        }
+        return res;
+    }
+
+}
